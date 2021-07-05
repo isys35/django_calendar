893b76b8259cd7c7b7d034ef7a03848d922713ca
isys35 <isys35@mail.ru>
2021-07-05 09:56:37 +0300

    add custom user

--- api/models.py
+++ api/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from api.manager import UserManager
+
 
 class MyUser(AbstractUser):
     USERNAME_FIELD = 'email'
     email = models.EmailField(unique=True)
     REQUIRED_FIELDS = []
+    objects = UserManager()
